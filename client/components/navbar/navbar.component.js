5bb48a817ca2196004da922ec4177b16464124e9
Hermaze <hermansoto@me.com>
2017-10-29 15:42:50 -0400

    initial commit

---
+++ client/components/navbar/navbar.component.js
@@ -0,0 +1,43 @@
+'use strict';
+/* eslint no-sync: 0 */
+
+import angular from 'angular';
+
+export class NavbarComponent {
+  menu = [{
+    title: 'HOME',
+    state: 'main'
+  }, {
+    title: 'ABOUT',
+    state: 'about'
+  }, {
+    title: 'SERVICES',
+    state: 'services'
+  }, {
+    title: 'MENU',
+    state: 'menu'
+  }, {
+    title: 'EVENTS',
+    state: 'events'
+  }];
+  isLoggedIn: Function;
+  isAdmin: Function;
+  getCurrentUser: Function;
+  isCollapsed = true;
+
+  constructor(Auth) {
+    'ngInject';
+
+    this.isLoggedIn = Auth.isLoggedInSync;
+    this.isAdmin = Auth.isAdminSync;
+    this.getCurrentUser = Auth.getCurrentUserSync;
+  }
+
+}
+
+export default angular.module('directives.navbar', [])
+  .component('navbar', {
+    template: require('./navbar.html'),
+    controller: NavbarComponent
+  })
+  .name;
